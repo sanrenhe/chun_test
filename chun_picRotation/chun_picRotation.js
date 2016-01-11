431f4b942e660523b10f5ea7bbb10a404267529f
sanrenhe <1015252387@qq.com>
2016-01-11 20:17:14 +0800

    图片轮播

--- chun_picRotation/chun_picRotation.js
+++ chun_picRotation/chun_picRotation.js
@@ -9,7 +9,12 @@ window.onload = function(){
 };
 
 function picRotation(){
-	this.pic_src = ["","","",""];
+	this.pic_src = [
+					"http://i.mmcdn.cn/simba/img/T117eTXmXqXXXXXXXX.jpg",
+					"http://img03.taobaocdn.com/tps/i3/T1t8eTXbBtXXXXXXXX-490-170.png",
+					"http://i.mmcdn.cn/simba/img/T1OVOUXeNjXXXXXXXX.jpg",
+					"http://i.mmcdn.cn/simba/img/T1J.9TXc8lXXXXXXXX.jpg"
+					];
 
 
 	this.init();
@@ -18,7 +23,6 @@ function picRotation(){
 picRotation.prototype = {
 	init : function(){
 		var _this = this;
-		console.log(_this.pic_src);
 		_this.createView(_this);
 	},
 	// 创建图片显示区域
@@ -27,20 +31,42 @@ picRotation.prototype = {
 		var picViewDiv = document.createElement("div");
 		picViewDiv.id = "picViewDiv";
 		document.body.appendChild(picViewDiv);
-		// div-ul
+		// div-ul(content)
 		var picUl = document.createElement("ul");
 		picUl.id = "picUl";
 		picViewDiv.appendChild(picUl);
-		// div-ul-li	div-ul-button
+		// div-ul-li(content)
 		for(var i=0; i<that.pic_src.length; i++){
 			var picLi = document.createElement("li");
-			picLi.src = that.pic_src[i];
+			picLi.className = "liContent";
+			picLi.style.left = "0px";
 			picUl.appendChild(picLi);
-			var picBtn = document.createElement("button");
-			picUl.appendChild(picBtn);
-		}
-		// div-button
+			// div-ul-li-img
+			var picLiImg = document.createElement("img");
+			picLiImg.setAttribute("src", that.pic_src[i]);
+			picLi.appendChild(picLiImg);
+		};
+		// div-ul(bottom)
+		var picUlBtn = document.createElement("ul");
+		picUlBtn.id = "picUlBtn";
+		picViewDiv.appendChild(picUlBtn);
+		// div-ul-li(bottom)
+		for(var j=that.pic_src.length-1; j>=0; j--){
+			var picLiBtn = document.createElement("li");
+			picLiBtn.className = "liBottom";
+			picLiBtn.innerHTML = (j+1).toString();
+			picUlBtn.appendChild(picLiBtn);
+		};
+		that.movePic(that);
+	},
+	// 动起来
+	movePic : function(that){
+		var $ul = document.getElementById("picUl");
+		var $li = $ul.getElementsByTagName("li");
 		
+	},
+	// 循环
+	loopPic : function(that, pics){
 
 	},
 }
