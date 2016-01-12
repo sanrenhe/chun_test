4a42beb11bc43a647672eaf4972a40c74b9b238e
sanrenhe <1015252387@qq.com>
2016-01-13 00:04:23 +0800

    图片轮播

--- chun_picRotation/chun_picRotation.js
+++ chun_picRotation/chun_picRotation.js
@@ -5,7 +5,8 @@
  * @date 2016/1/10
  */
 window.onload = function(){
-	new picRotation();
+	var pic = new picRotation();
+	pic.init();
 };
 
 function picRotation(){
@@ -13,20 +14,24 @@ function picRotation(){
 					"http://i.mmcdn.cn/simba/img/T117eTXmXqXXXXXXXX.jpg",
 					"http://img03.taobaocdn.com/tps/i3/T1t8eTXbBtXXXXXXXX-490-170.png",
 					"http://i.mmcdn.cn/simba/img/T1OVOUXeNjXXXXXXXX.jpg",
-					"http://i.mmcdn.cn/simba/img/T1J.9TXc8lXXXXXXXX.jpg"
+					"http://i.mmcdn.cn/simba/img/T1J.9TXc8lXXXXXXXX.jpg",
+					"http://i.mmcdn.cn/simba/img/T117eTXmXqXXXXXXXX.jpg",
+					"http://img03.taobaocdn.com/tps/i3/T1t8eTXbBtXXXXXXXX-490-170.png",
 					];
-
-
-	this.init();
+	this.j = 0;
+	this.count = 0;
+	this.Width = 0;
 };
 
 picRotation.prototype = {
 	init : function(){
-		var _this = this;
-		_this.createView(_this);
+		var that = this;
+		that.createView(that);
+		return that;
 	},
 	// 创建图片显示区域
-	createView : function(that){
+	createView : function(){
+		var that = this;
 		// div
 		var picViewDiv = document.createElement("div");
 		picViewDiv.id = "picViewDiv";
@@ -40,6 +45,7 @@ picRotation.prototype = {
 			var picLi = document.createElement("li");
 			picLi.className = "liContent";
 			picLi.style.left = "0px";
+			picLi.id = i+1;
 			picUl.appendChild(picLi);
 			// div-ul-li-img
 			var picLiImg = document.createElement("img");
@@ -51,23 +57,37 @@ picRotation.prototype = {
 		picUlBtn.id = "picUlBtn";
 		picViewDiv.appendChild(picUlBtn);
 		// div-ul-li(bottom)
-		for(var j=that.pic_src.length-1; j>=0; j--){
+		for(var j=that.pic_src.length-3; j>=0; j--){
 			var picLiBtn = document.createElement("li");
 			picLiBtn.className = "liBottom";
 			picLiBtn.innerHTML = (j+1).toString();
 			picUlBtn.appendChild(picLiBtn);
 		};
-		that.movePic(that);
+		that.movePic();
+		return that;
 	},
 	// 动起来
-	movePic : function(that){
+	movePic : function(){
+		var that = this;
 		var $ul = document.getElementById("picUl");
-		var $li = $ul.getElementsByTagName("li");
-		
+		that.picloop = window.setInterval(function(){
+			that.loopPic($ul);
+		},2000);
+		return that;
 	},
 	// 循环
-	loopPic : function(that, pics){
-
+	loopPic : function(pics){
+		var that = this;
+		if(this.count > that.pic_src.length-3){
+			pics.setAttribute("style", 'transition-duration: 300ms; transform: translate3d(' + that.Width + 'px, 0px, 0px)');
+			this.count = 0;
+			that.j = 400;
+		}
+		that.j = that.j - pics.firstChild.offsetWidth;
+		that.Width = pics.firstChild.offsetWidth * that.pic_src.length;
+		pics.setAttribute("style", 'width:' + that.Width + 'px;' + 'transition-duration: 300ms; transform: translate3d(' + that.j + 'px, 0px, 0px)');
+		this.count++;
+		return that;
 	},
 }
 
