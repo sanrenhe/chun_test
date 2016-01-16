ff2292f9b7d0699196317c1db3391ba91d3835fb
sanrenhe <1015252387@qq.com>
2016-01-16 17:10:09 +0800

    图片轮播V2

--- chun_picRotation/V2/chun_picXuan.js
+++ chun_picRotation/V2/chun_picXuan.js
@@ -14,7 +14,7 @@ function picXuan(){
 					"http://i.mmcdn.cn/simba/img/T117eTXmXqXXXXXXXX.jpg",
 					"http://img03.taobaocdn.com/tps/i3/T1t8eTXbBtXXXXXXXX-490-170.png",
 					"http://i.mmcdn.cn/simba/img/T1OVOUXeNjXXXXXXXX.jpg",
-					"http://i.mmcdn.cn/simba/img/T1J.9TXc8lXXXXXXXX.jpg"
+					"http://i.mmcdn.cn/simba/img/T1J.9TXc8lXXXXXXXX.jpg",
 					];
 	this.j = 0;
 	this.count = 0;
@@ -42,22 +42,57 @@ picXuan.prototype = {
 		for(var i=0; i<that.pic_src.length; i++){
 			var xuan_div_ul_li = document.createElement("li");
 			xuan_div_ul_li.className = "xuanLi";
+			xuan_div_ul_li.id = "xuanLi" + i;
 			xuan_div_ul.appendChild(xuan_div_ul_li);
 			// div-ul-li-img
 			var xuan_div_ul_li_img = document.createElement("img");
+			xuan_div_ul_li_img.id = "img" + i;
 			xuan_div_ul_li_img.setAttribute("src", that.pic_src[i]);
 			xuan_div_ul_li.appendChild(xuan_div_ul_li_img);
 		};
+		that.time = window.setInterval(function(){
+			that.Xuan();
+		},4000);
 		
-
-
-
-
-
-
-
-
-
 		return that;
-	}
+	},
+	Xuan : function(){
+		var that = this;
+		// window.clearTimeout(that.time);
+		var $img = new Array,
+			$xuanLi = new Array;
+		for(var i=0; i<that.pic_src.length; i++){
+			$img[i] = document.getElementById("img" + i);
+			$xuanLi[i] = document.getElementById("xuanLi" + i);
+		};
+		$xuanLi[0].setAttribute("style", "transition-duration: 2000ms;transform: translate3d(-210px,-120px,0px);");
+		$img[0].setAttribute("style", "transition: width 2s, height 2s; height: 160px; width: 120px");
+		$xuanLi[1].setAttribute("style", "transition-duration: 2000ms;transform: translate3d(210px,-160px,0px);");
+		$xuanLi[2].setAttribute("style", "transition-duration: 2000ms;transform: translate3d(210px,160px,0px);");
+		$xuanLi[3].setAttribute("style", "transition-duration: 2000ms;transform: translate3d(-210px,120px,0px);");
+		$img[3].setAttribute("style", "transition: width 2s, height 2s; height: 200px; width: 150px");
+		
+		window.setTimeout(function(){
+			for(var i=0; i<that.pic_src.length; i++){
+				$img[i].removeAttribute("style");
+				$xuanLi[i].removeAttribute("style");
+			};
+			that.Huan($img,$xuanLi);
+		},2000);
+		return that;
+	},
+	Huan : function($img, $xuanLi){
+		var that = this;
+		console.log("chun");
+		for(var i=0; i<that.pic_src.length; i++){
+			if(i<3){
+				$img[i].id = "img" + (i+1);
+				$xuanLi[i].id = "xuanLi" + (i+1);
+			}else{
+				$img[i].id = "img0";
+				$xuanLi[i].id = "xuanLi0";
+			}
+		};
+		return that;
+	},
 }
