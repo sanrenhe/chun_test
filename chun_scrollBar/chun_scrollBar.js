d62c4b61a8c7ba4c94f4b9e597332b046105b9d9
sanrenhe <1015252387@qq.com>
2016-01-24 21:55:59 +0800

    js原生滚动条

--- chun_scrollBar/chun_scrollBar.js
+++ chun_scrollBar/chun_scrollBar.js
@@ -5,7 +5,9 @@
  * @date 2016-1-23 
  */
 function scrollBar(){
+	this.option = {
 
+	};
 };
 
 scrollBar.prototype = {
@@ -23,14 +25,14 @@ scrollBar.prototype = {
 		var that = this;
 		var barDiv = document.createElement('div');
 		barDiv.id = 'barDiv';
-		barDiv.style.position = 'relative';
-		barDiv.style.cssFloat = 'right';
+		barDiv.style.position = 'absolute';
+		barDiv.style.left = document.body.offsetWidth-20 + 'px';
 		barDiv.style.top = '0px';
 		barDiv.style.border = '1px solid black';
 		barDiv.style.width = '20px';
 		barDiv.style.height = document.body.offsetHeight + 'px';
 		barDiv.style.zIndex = '99999';
-		document.body.appendChild(barDiv);
+		document.getElementById('slider').appendChild(barDiv);
 
 
 
