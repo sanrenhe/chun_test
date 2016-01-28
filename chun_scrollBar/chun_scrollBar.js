2166976aff003e0fd58e999f182636d6fe0a287a
sanrenhe <1015252387@qq.com>
2016-01-28 18:20:18 +0800

    js原生滚动条

--- chun_scrollBar/chun_scrollBar.js
+++ chun_scrollBar/chun_scrollBar.js
@@ -4,100 +4,94 @@
  * @email chunzhen@leju.com
  * @date 2016-1-23 
  */
-function scrollBar(){
-	this.option = {
-
+function loadjscssfile(filename, filetype){
+	if(filename == '' || filetype == ''){
+		console.log('引入错误！');
+		return false;
 	};
+	if(filetype == 'js'){
+		var fileScript = document.createElement('script');
+		fileScript.setAttribute('type','text/javascript');
+		fileScript.setAttribute('src',filename);
+		document.body.appendChild(fileScript);
+	}else if(filetype == 'css'){
+		var fileLink = document.createElement('link');
+		fileLink.setAttribute('rel','stylesheet');
+		fileLink.setAttribute('type','text/css');
+		fileLink.setAttribute('href',filename);
+		document.head.appendChild(fileLink);
+	}else{
+		console.log('无效引入');
+		return false;
+	};
+};
+
+function scrollBar(){
+	this.$body = document.body;
+	this.$scrollBar1 = document.getElementById("scrollBar1");
+	this.$viewport = document.getElementById("viewport");
+	this.$overview = document.getElementById("overview");
+	this.scrollFlag = false;
 };
 
 scrollBar.prototype = {
 	init : function(){
 		var that = this;
-		that.$slider = document.getElementById("slider");
-		that.$slider.style.height = '500px';
-		that.$slider.style.overflow = 'hidden';
-		that.$slider.style.margin = '0px';
-		that.$firstChild = that.$slider.firstElementChild;
-		console.log(that.$firstChild);
+		that.$viewport.style.height = '500px';
+		that.$viewport.style.overflow = 'hidden';
+		that.$viewport.style.margin = '0px';
 		that.createBar();
-
-
-
-
 		return that;
 	},
 	createBar : function(){
 		var that = this;
-		var barDiv = document.createElement('div');
-		barDiv.id = 'barDiv';
-		barDiv.style.position = 'absolute';
-		barDiv.style.left = that.$slider.offsetWidth-20 + 'px';
-		barDiv.style.top = '0px';
-		barDiv.style.border = '1px solid black';
-		barDiv.style.width = '20px';
-		barDiv.style.height = that.$slider.offsetHeight + 'px';
-		barDiv.style.zIndex = '4';
-		barDiv.style.textAlign = 'center';
-		barDiv.style.paddingTop = '1px';
-		that.$slider.appendChild(barDiv);
+		var $scrollBarBody = document.createElement('div');
+		$scrollBarBody.id = 'scrollBar';
+		$scrollBarBody.style.left = that.$viewport.offsetWidth-20 + 'px';
+		$scrollBarBody.style.height = that.$viewport.offsetHeight + 'px';
+		that.$scrollBar1.appendChild($scrollBarBody);
 
-		var barDivUl = document.createElement('ul');
-		barDivUl.style.top = '1px';
-		barDivUl.style.position = 'relative';
-		barDivUl.style.border = '1px solid grey';
-		barDivUl.style.paddingLeft = '0px';
-		barDivUl.style.width = (parseInt(barDiv.offsetWidth)-6) + 'px';
-		barDivUl.style.height = that.$slider.offsetHeight*0.2 + 'px';
-		barDivUl.style.margin = '0 auto';
-		barDivUl.style.zIndex = '5';
-		barDivUl.style.borderRadius = '5px';
-		barDivUl.style.backgroundColor = '#3399FF';
-		barDivUl.style.cursor = 'pointer';
-		barDiv.appendChild(barDivUl);
+		var $track = document.createElement('div');
+		$track.id = 'track';
+		$track.style.height = that.$viewport.offsetHeight*0.2 + 'px';
+		$scrollBarBody.appendChild($track);
 
 		var start=distance=end=0;
-		barDivUl.addEventListener("mousedown", put, false);
+		$track.addEventListener("mousedown", put, false);
 		function put(e){
 			e.stopPropagation();
-			document.body.setAttribute("style","user-select: none; -o-user-select: none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none;");
+			that.shakeFlag = true;
+			that.$body.className = "noSelect";
 			start = e.pageY;
-			document.body.addEventListener("mousemove", move, false);
-			barDivUl.addEventListener("mousemove", move, false);
+			that.$body.addEventListener("mousemove", move, false);
+			$track.addEventListener("mousemove", move, false);
 			function move(evt){
-				evt.stopPropagation();
-				end = evt.pageY;
-				var startTop = parseInt(barDivUl.style.top);
-				distance = end - startTop;
-				barDivUl.style.top = end-40 + 'px';
-				console.log("move");
+				if(that.shakeFlag){
+					evt.stopPropagation();
+					end = evt.pageY;
+					var startTop = parseInt($track.style.top);
+					distance = end - startTop;
+					$track.style.top = end-40 + 'px';
+					that.$overview.style.top = -parseInt($track.style.top) + "px";
+					console.log(that.$overview.style.top);
+				}
 			};
-			document.body.addEventListener("mouseup", up, false);
-			barDivUl.addEventListener("mouseup", up, false);
+			that.$body.addEventListener("mouseup", up, false);
+			$track.addEventListener("mouseup", up, false);
 			function up(e){
 				e.stopPropagation();
-				barDivUl.style.top = e.pageY-40 + 'px';
-				document.body.removeEventListener("mousemove", move, false);
-				barDivUl.removeEventListener("mousemove", move, false);
-				document.body.setAttribute("style","");
-
-				// barDivUl.removeEventListener("mousedown", put, false);
-				// barDivUl.style.top = e.pageY-40 + 'px';
-				console.log("up");
+				that.shakeFlag = false;
+				that.$body.className = "";
 			};
-			document.body.addEventListener("mouseup", up, false);
 			console.log("down");
-			
-			// debugger;
 		};
-		
-
 		return that;
 	},
-}
+};
 
 var sBar = new scrollBar();
 sBar.init();
-
+loadjscssfile('chun_scrollBar.css','css');
 
 
 
