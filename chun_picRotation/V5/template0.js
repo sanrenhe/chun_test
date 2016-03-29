448aacfbc2fa734ce790ae8a6c57171d74005c04
sanrenhe <1015252387@qq.com>
2016-03-29 17:04:50 +0800

    图片滑动第五版
    使用slider插件控制滑动
    使用seajs框架进行开发

---
+++ chun_picRotation/V5/template0.js
@@ -0,0 +1,32 @@
+define(function(require, exports, module) {
+    var $ = require("zepto");
+    var slider = require("slider");
+    var sum = $("#numList").children().length;
+    var scrLeft = 300;
+
+    new slider("#container", {
+        wrap: "#wrapper",
+        trigger: "#numList",
+        activeTriggerCls: "d-cur",
+        hasTrigger: true,
+        prev: ".left",
+        next: ".right",
+        callback: function(index, tag) {
+            if (tag == 1 && index < 5) {
+                $("#info_louceng").html($(".d-cur").next().data("info"));
+                // $(".top").scrollLeft(scrLeft);
+                // scrLeft = scrLeft + 300;
+            } else if (tag == -1 && index >= 1) {
+                $("#info_louceng").html($(".d-cur").prev().data("info"));
+                // $(".top").scrollLeft(scrLeft);
+                // scrLeft = scrLeft - 600 < 0 ? 0 : scrLeft - 600;
+            }
+            $("#info_num").html((index + 1) + '/' + sum);
+        }
+    });
+
+    $("#numList li").on("click", function() {
+        $("#info_louceng").html($(this).data("info"));
+        $("#info_num").html($(this).html() + '/' + sum);
+    });
+})
